fb0b0f2439efc1b49fa69e875a5595b93de230f2
Patricia Monteiro <p.alexandracesar@gmail.com>
2025-06-18 12:29:01 +0100

    Added fill_blank

---
+++ srcs/parsing/fill_blank.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fill_blank.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palexand <palexand@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 12:22:09 by palexand          #+#    #+#             */
+/*   Updated: 2025/06/18 12:22:30 by palexand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incs/cub3d.h"
+
+void	fill_blank(t_data *data, int x, int y)
+{
+	if (x < 0 || y < 0 || x >= data->map->max_height - 1
+		|| y > (int)(ft_strlen(data->map->map[x]) - 1))
+		return ;
+	if (data->map->map[x][y] == '1' || data->map->map[x][y] == '0')
+		return ;
+	if (data->map->map[x][y] == ' ')
+		data->map->map[x][y] = '0';
+	fill_blank(data, x + 1, y);
+	fill_blank(data, x - 1, y);
+	fill_blank(data, x, y + 1);
+	fill_blank(data, x, y - 1);
+}
